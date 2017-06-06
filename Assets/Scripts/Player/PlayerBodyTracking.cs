e9594a53012905abc5dfd36432db85d276db1324
Tom Gurion <tgurion@inition.co.uk>
2017-06-06 12:44:56 +0100

    Hands follow the controllers localy

---
+++ Assets/Scripts/Player/PlayerBodyTracking.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerBodyTracking : NetworkBehaviour {
+
+	public string trackingDeviceTag;
+	public Transform gameObjectTransform;
+	private Transform trackingDeviceTransform;
+
+
+	void Start ()
+	{
+		trackingDeviceTransform = GameObject.FindGameObjectWithTag (trackingDeviceTag).GetComponent<Transform> ();
+	}
+	
+
+	void Update ()
+	{
+		if (isLocalPlayer)
+		{
+			gameObjectTransform.position = trackingDeviceTransform.position;
+			gameObjectTransform.rotation = trackingDeviceTransform.rotation;
+		}
+	}
+}
