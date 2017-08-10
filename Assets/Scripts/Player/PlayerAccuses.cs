8d5240b8e2821af8196861e489db68f0bc0fe467
Tom Gurion <tgurion@inition.co.uk>
2017-08-10 10:09:47 +0100

    Add audio indication for accusation

--- Assets/Scripts/Player/PlayerAccuses.cs
+++ Assets/Scripts/Player/PlayerAccuses.cs
@@ -7,13 +7,22 @@ using UnityEngine.Networking;
 public class PlayerAccuses : NetworkBehaviour {
 
 	public string controllerTag;
+	public AudioClip accusingNothing;
+	public AudioClip accusingPlayer;
 
 	private PlayerApproaches playerApproaches;
 	private SteamVR_TrackedObject trackedObj;
+	private AudioSource audioSource;
 
 	private SteamVR_Controller.Device Controller
 	{
 		get { return SteamVR_Controller.Input((int) trackedObj.index); }
+	}
+
+
+	private void Awake()
+	{
+		audioSource = GetComponent<AudioSource> ();
 	}
 
 
@@ -28,21 +37,15 @@ public class PlayerAccuses : NetworkBehaviour {
 
 
 	private void Update () {
-		if (isLocalPlayer && null != playerApproaches.attention)
+		if (isLocalPlayer)
 		{
-			if (SceneManager.GetActiveScene ().name == "Simulator")
+			if (SceneManager.GetActiveScene ().name == "Simulator" && Input.GetButtonDown ("Accuse"))
 			{
-				if (Input.GetButtonUp ("Accuse"))
-				{
-					StartCoroutine (AccusePlayerForAutopiloting (playerApproaches.attention));
-				}
+				StartCoroutine (AccusePlayerForAutopiloting (playerApproaches.attention));
 			}
-			else
+			else if (Controller.GetHairTriggerDown ())
 			{
-				if (Controller.GetHairTriggerDown ())
-				{
-					StartCoroutine (AccusePlayerForAutopiloting (playerApproaches.attention));
-				}
+				StartCoroutine (AccusePlayerForAutopiloting (playerApproaches.attention));
 			}
 		}
 	}
@@ -50,12 +53,23 @@ public class PlayerAccuses : NetworkBehaviour {
 
 	private IEnumerator AccusePlayerForAutopiloting(PlayerController otherPlayer)
 	{
-		bool correct = (otherPlayer.state == "autopilot");
-		string prefix = correct ? "Rightfully" : "Mistakenly";
-		Debug.Log (prefix + " accusing " + otherPlayer.netId.Value + " for autopiloting");
+		if (null == otherPlayer)
+		{
+			audioSource.clip = accusingNothing;
+		}
+		else
+		{
+			audioSource.clip = accusingPlayer;
 
-		// Send the message to the server that I'm not autopiloting anymore
-		string conclusion = correct ? "correct" : "incorrect";
-		yield return new WWW(NetworkGui.serversAddress + ":8080/" + netId.Value + "/accuse/" + otherPlayer.netId.Value + "/" + conclusion);
+			bool correct = (otherPlayer.state == "autopilot");
+			string prefix = correct ? "Rightfully" : "Mistakenly";
+			Debug.Log (prefix + " accusing " + otherPlayer.netId.Value + " for autopiloting");
+
+			// Send the message to the server that I'm not autopiloting anymore
+			string conclusion = correct ? "correct" : "incorrect";
+			yield return new WWW(NetworkGui.serversAddress + ":8080/" + netId.Value + "/accuse/" + otherPlayer.netId.Value + "/" + conclusion);
+		}
+
+		audioSource.Play ();
 	}
 }
