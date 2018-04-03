57f8ea94b096483336f475d916ac4d05f3606c52
Tom Gurion <nagasaki45@gmail.com>
2018-04-03 15:30:27 +0100

    Implement the backchannels TCP client

--- Assets/Scripts/Player/PlayerExpectsBackchannel.cs
+++ Assets/Scripts/Player/PlayerExpectsBackchannel.cs
@@ -1,9 +1,16 @@
+using System.Collections;
+using System.Net.Sockets;
 using UnityEngine;
 
-public class PlayerExpectsBackchannel : MonoBehaviour, IMicrophoneSubscriber
+public class PlayerExpectsBackchannel : MonoBehaviour
 {
+    public string tcpServerAddress;
+    public int tcpServerPort;
+
     private PubSubClient pubSubClient;
     private PlayerTalking playerTalking;
+    private TcpClient tcpClient;
+    private NetworkStream networkStream;
 
 
     void Start()
@@ -12,10 +19,43 @@ public class PlayerExpectsBackchannel : MonoBehaviour, IMicrophoneSubscriber
         playerTalking = GetComponent<PlayerTalking>();
     }
 
-
-    void ReceiveBang()
+    void Awake()
     {
-        Debug.Log("Player expects backchannel");
-        pubSubClient.CmdPublish("backchannels");
+        // Connect to TCP server
+        tcpClient = new TcpClient(tcpServerAddress, tcpServerPort);
+        networkStream = tcpClient.GetStream();
+
+        StartCoroutine(PollStream());
+    }
+
+
+    void Update()
+    {
+        byte message = (byte) (playerTalking.isTalking ? 1 : 0);
+        networkStream.WriteByte(message);
+    }
+
+
+    IEnumerator PollStream()
+    {
+        while (true)
+        {
+            if (networkStream.DataAvailable)
+            {
+                byte[] throwaway = new byte[1024];
+                networkStream.Read(throwaway, 0, throwaway.Length);
+                Debug.Log("Player expects backchannel");
+                pubSubClient.CmdPublish("backchannels");
+            }
+            // Let the engine run for a frame.
+            yield return null;
+        }
+    }
+
+
+    void OnDestroy()
+    {
+        networkStream.Close();
+        tcpClient.Close();
     }
 }
