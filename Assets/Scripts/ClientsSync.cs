2d2172edb329531118ca5bbbcc5420a16fe7c9b5
Tom Gurion <nagasaki45@gmail.com>
2018-04-07 14:29:54 +0100

    Sync clients (write to log and flash screen) by pressing 's'. Fix #23

---
+++ Assets/Scripts/ClientsSync.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class ClientsSync : NetworkBehaviour
+{
+    FlashScreen flashScreen;
+
+    void Start()
+    {
+        flashScreen = GameObject.FindGameObjectWithTag ("FlashScreen").GetComponent<FlashScreen> ();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("ClientsSync"))
+        {
+            CmdSync();
+        }
+    }
+
+
+    [Command]
+    void CmdSync()
+    {
+        RpcSync();
+    }
+
+
+    [ClientRpc]
+    void RpcSync()
+    {
+        Debug.Log("Clients sync (screen flashes)");
+        flashScreen.Flash ();
+    }
+}
