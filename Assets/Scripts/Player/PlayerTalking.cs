affdf57d409be7e85353ce2a8679a5ed3c73d0c4
Tom Gurion <nagasaki45@gmail.com>
2018-12-20 16:46:50 +0000

    Add speaker analysis based on isTalking state

--- Assets/Scripts/Player/PlayerTalking.cs
+++ Assets/Scripts/Player/PlayerTalking.cs
@@ -12,19 +12,47 @@ public class PlayerTalking : NetworkBehaviour {
     [SyncVar]
     public bool isTalking;
 
-    public float talkingAmplitudeThreshold;
+    [SyncVar]
+    public bool speaker;
 
-    private DissonanceComms comms;
+    public double talkingRatio;
+
+    public float talkingAmplitudeThreshold;
+    public int sampleRate;
+    public float lowPassCutoffFrequency;
+
+    DissonanceComms comms;
+
+    // Only on the server
+    Interpolator interpolator;
+    Butterworth lowPassFilter;
 
 
     private void Start()
     {
         comms = GameObject.FindGameObjectWithTag ("DissonanceSetup").GetComponent<DissonanceComms> ();
+
+        if (isServer)
+        {
+            interpolator = new Interpolator(1.0 / sampleRate);
+            lowPassFilter = new Butterworth(lowPassCutoffFrequency, sampleRate, Butterworth.PassType.Lowpass);
+        }
     }
 
 
     private void Update ()
     {
+        // Update talking ratio on the server
+        if (isServer)
+        {
+            float now = Time.time;
+            foreach (double val in interpolator.Interpolate(now, isTalking ? 1.0 : 0.0))
+            {
+                talkingRatio = lowPassFilter.Filter(val);
+            }
+        }
+
+        // Local players to update isTalking
         if (!isLocalPlayer)
         {
             return;
