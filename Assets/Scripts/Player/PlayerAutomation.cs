0d365709f74bc0bddec595ce1e6bd781bd2681cf
Tom Gurion <nagasaki45@gmail.com>
2018-12-27 15:39:11 +0000

    While automated filter out head movement between 1 and 4 hertz

--- Assets/Scripts/Player/PlayerAutomation.cs
+++ Assets/Scripts/Player/PlayerAutomation.cs
@@ -9,6 +9,7 @@ public class PlayerAutomation : MonoBehaviour {
     public string[] automationModels;
     public string[] animations;
     public float animationCrossfadeSeconds;
+    public float lerp;
 
     public string automationModel;
     public Transform trueHead;
@@ -17,18 +18,63 @@ public class PlayerAutomation : MonoBehaviour {
 
     private float lastAutomationFinished = 0.0f;
 
+    // Notch in head movement data between 1 and 4 hertz
+    private int sampleRate = 100;
+    private float lowPassFreq = 1;
+    private float highPassFreq = 4;
+    private Interpolator yInterpolator;
+    private Butterworth yLowpassFilter;
+    private Butterworth yHighpassFilter;
+    private Interpolator pitchInterpolator;
+    private Butterworth pitchLowpassFilter;
+    private Butterworth pitchHighpassFilter;
+
+
+    void Start()
+    {
+        yInterpolator = new Interpolator(1.0 / sampleRate);
+        yLowpassFilter = new Butterworth(lowPassFreq, sampleRate, Butterworth.PassType.Lowpass);
+        yHighpassFilter = new Butterworth(lowPassFreq, sampleRate, Butterworth.PassType.Highpass);
+        pitchInterpolator = new Interpolator(1.0 / sampleRate);
+        pitchLowpassFilter = new Butterworth(lowPassFreq, sampleRate, Butterworth.PassType.Lowpass);
+        pitchHighpassFilter = new Butterworth(lowPassFreq, sampleRate, Butterworth.PassType.Highpass);
+    }
+
 
     void Update()
     {
+        float now = Time.time;
+        Vector3 targetPosition;
+        Quaternion targetRotation;
+
+        // Always interpolate true head values so interpolator won't jump
+        double y = (double) trueHead.position.y;
+        double pitch = (double) trueHead.eulerAngles.x;
+        pitch = (pitch + 90) % 360;  // So there's no 0 to cross
+        foreach (double interpolated in yInterpolator.Interpolate(now, y))
+        {
+            // Notch by summing low pass and high pass
+            y = yLowpassFilter.Filter(interpolated) + yHighpassFilter.Filter(interpolated);
+        }
+        foreach (double interpolated in pitchInterpolator.Interpolate(now, pitch))
+        {
+            // Notch by summing low pass and high pass
+            pitch = pitchLowpassFilter.Filter(interpolated) + pitchHighpassFilter.Filter(interpolated);
+        }
+
         if (!IsAutomated())
         {
-            remoteHead.position = trueHead.position;
-            remoteHead.rotation = trueHead.rotation;
+            targetPosition = trueHead.position;
+            targetRotation = trueHead.rotation;
         }
         else
         {
-            // TODO
+            targetPosition = new Vector3(trueHead.position.x, (float) y, trueHead.position.z);
+            targetRotation = Quaternion.Euler((float) pitch - 90, trueHead.eulerAngles.y, trueHead.eulerAngles.z);
         }
+
+        remoteHead.position = Vector3.Lerp(remoteHead.position, targetPosition, lerp * Time.deltaTime);
+        remoteHead.rotation = Quaternion.Lerp(remoteHead.rotation, targetRotation, lerp * Time.deltaTime);
     }
 
 
